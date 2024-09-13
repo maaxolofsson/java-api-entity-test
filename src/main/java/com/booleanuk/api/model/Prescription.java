13c03cce9d442c9e2239adf12373b251ecd6ff13
Max Valdaserides Olofsson <mv222se@student.lnu.se>
2024-09-13 13:18:15 +0200

    Added member variables and joining tables in prescription

--- src/main/java/com/booleanuk/api/model/Prescription.java
+++ src/main/java/com/booleanuk/api/model/Prescription.java
@@ -1,4 +1,34 @@
 package com.booleanuk.api.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "prescriptions")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class Prescription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private int quantity;
+
+    @ManyToMany
+    @JoinColumn(name = "medicine_id")
+    private List<Medicine> medicines;
+
+    public void addMedicine(Medicine m) {
+        this.medicines.add(m);
+    }
+
 }
