2ca5857ac8f4936b927f873cd435fd966973bc87
Max Valdaserides Olofsson <mv222se@student.lnu.se>
2024-09-13 09:21:07 +0200

    added create and getall in appointment controller

--- src/main/java/com/booleanuk/api/controller/AppointmentController.java
+++ src/main/java/com/booleanuk/api/controller/AppointmentController.java
@@ -1,7 +1,11 @@
 package com.booleanuk.api.controller;
 
 import com.booleanuk.api.model.Appointment;
+import com.booleanuk.api.model.Doctor;
+import com.booleanuk.api.model.Patient;
 import com.booleanuk.api.repository.AppointmentRepository;
+import com.booleanuk.api.repository.DoctorRepository;
+import com.booleanuk.api.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,8 +21,27 @@ public class AppointmentController {
     @Autowired
     private AppointmentRepository appointments;
 
+    @Autowired
+    private PatientRepository patients;
+
+    @Autowired
+    private DoctorRepository doctors;
+
     @PostMapping
     public ResponseEntity<Appointment> create(@RequestBody Appointment toAdd) {
+        Doctor doctor = this.doctors.findById(toAdd.getDoctor().getId()).
+                orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+                );
+
+        Patient patient = this.patients.findById(toAdd.getPatient().getId()).
+                orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+                );
+
+        toAdd.setDoctor(doctor);
+        toAdd.setPatient(patient);
+
         return new ResponseEntity<>(this.appointments.save(toAdd), HttpStatus.CREATED);
     }
 
