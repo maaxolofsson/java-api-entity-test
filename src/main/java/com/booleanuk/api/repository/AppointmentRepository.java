d8fb0d35f8e98a11bd9c2a82eec3bafb5dee5eb9
Max Valdaserides Olofsson <mv222se@student.lnu.se>
2024-09-13 09:19:38 +0200

    added find appointments by doctor id

--- src/main/java/com/booleanuk/api/repository/AppointmentRepository.java
+++ src/main/java/com/booleanuk/api/repository/AppointmentRepository.java
@@ -3,6 +3,8 @@ package com.booleanuk.api.repository;
 import com.booleanuk.api.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
-    public Appointment findAllByDoctorId(int doctorId);
+    public List<Appointment> findAllBydoctorId(int doctorId);
 }
